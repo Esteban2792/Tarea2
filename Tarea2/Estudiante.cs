54496779c419fd1a4e09f73b6634d32683705dd1
Esteban2792 <85144202+Esteban2792@users.noreply.github.com>
2022-02-18 22:51:13 -0600

    Agregar archivos de proyecto.

---
+++ Tarea2/Estudiante.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Estudiante
+    {
+        
+        
+            //ATRIBUTOS
+            string[] cedula = new string[5];
+            string[] nombre = new string[5];
+        double[] promedio = new double[5];
+        string[] condicion = new string[5];
+            int pos;
+
+            //CONSTRUCTOR
+            public Estudiante() // constructor 
+            {
+                pos = 0;
+            }
+            public Estudiante(int posicion) // constructor 
+            {
+                pos = posicion;
+            }
+
+            public void InicializarVectores()
+            {
+                for (int i = 0; i < cedula.Length; i++)
+                {
+                    cedula[i] = "";
+                    nombre[i] = "";
+                promedio[i] = 0;
+                condicion[i] = "";
+                }
+                Console.WriteLine("Arreglos inicializados...");
+
+            }
+            public void IncluirEstudiante()
+            {
+                char opcion;
+                do
+                {
+                    Console.WriteLine("Digite su nombre: ");
+                    nombre[pos] = Console.ReadLine();
+                    Console.WriteLine("Digite su cedula");
+                    cedula[pos] = Console.ReadLine();
+                Console.WriteLine("Digite su promedio del Estudiante");
+                promedio[pos] = double.Parse(Console.ReadLine());
+                if (promedio[pos] >= 70)
+                {
+                    condicion[pos] = "Aprobado";
+                } else
+                {
+                    condicion[pos] = "Desaprobado";
+                }
+                pos++;
+                    Console.WriteLine("Desea continuar s/n");
+                    opcion = Convert.ToChar(Console.ReadLine().ToUpper());
+                } while (opcion != 'N');
+            }
+
+            
+            public void ConsultarEstudiante()
+            {
+                Boolean existe = false;
+
+                Console.WriteLine("Digite la cedula: ");
+                string documento = Console.ReadLine();
+                for (int i = 0; i < cedula.Length; i++)
+                {
+                    if (documento.Equals(cedula[i]))
+                    {
+                        Console.WriteLine($"El nombre del estudiante es {nombre[i]} y su promedio es {promedio[i]}");
+                        existe = true;
+                    }
+                }
+                if (!existe)
+                {
+                    Console.WriteLine("EL Estudiante no existe");
+                    Console.WriteLine();
+                }
+            }
+
+
+            public void ModificarEstudiante()
+            {
+            Console.WriteLine("Digite la cedula: ");
+                string documento = Console.ReadLine();
+                byte indice = 0;
+                while (indice <= cedula.Length && !documento.Equals(cedula[indice]))
+                {
+                    indice++;
+                }
+
+                if (indice > cedula.Length)
+                {
+                    Console.WriteLine(" Cedula no existe");
+                }
+                else
+                {
+                    Console.WriteLine("Digite un nuevo nombre:");
+                    nombre[indice] = Console.ReadLine();
+                    Console.WriteLine("Digite una nueva cedula");
+                    cedula[indice] = Console.ReadLine();
+                Console.WriteLine("Digite una nuevo promedio");
+                promedio[indice] = double.Parse(Console.ReadLine());
+                
+
+
+                }
+            }
+            public void BorrarEstudiante() {
+            Console.WriteLine("Ingrese la cedula del estudiante: ");
+            string codigo = Console.ReadLine();
+            for (int i = 0; i < cedula.Length; i++)
+            {
+                if (cedula[i].Equals(codigo))
+                {
+                    nombre[i] = "";
+                    cedula[i] = "";
+                    condicion[i] = "";
+                    promedio[i]=0;
+                    Console.WriteLine("Estudiante Borrado");
+                }
+            }
+        }
+
+        public void ReporteEstudianteCondicion()
+        {
+            Console.WriteLine("Nombre    Condición ");
+            Console.WriteLine("-------------------- ");
+            for (int i = 0; i < cedula.Length; i++)
+            {
+                if (promedio[i] >= 70)
+                {
+                    Console.WriteLine($"{ nombre[i]}    {condicion[i]} ");
+
+                }
+                else
+                {
+
+                    
+
+                    Console.WriteLine($"{ nombre[i]}    {condicion[i]} ");
+
+                }
+            }
+            
+        }
+
+        public void ReporteEstudiante()
+        {
+            Console.WriteLine(" Cedula      Nombre        Promedio     Condición ");
+            Console.WriteLine("-------------------------------------------------- ");
+            for (int i = 0; i < cedula.Length; i++)
+            {
+                if (cedula[i].Length != 0)
+                {
+                    Console.WriteLine($"{cedula[i]}  { nombre[i]}   { promedio[i]}  {condicion[i]} ");
+
+                }
+            }
+        }
+    }
+    
+    }
+
---
+++ Tarea2/Menu.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+
+    class Menu
+    {
+        static Estudiante estudiante = new Estudiante();
+
+        public static void principal()
+        {
+            //Estudiante estudiante = new Estudiante();
+
+            int opcion;
+            do
+            {
+                Console.WriteLine("****** MENU PRINCIPAL ******");
+                Console.WriteLine("1- Inicializar Arreglos");
+                Console.WriteLine("2- Incluir Estudiantes");
+                Console.WriteLine("3- Consultar Estudiantes");
+                Console.WriteLine("4- Modificar Estudiantes");
+                Console.WriteLine("5- Eliminar Estudiantes");
+                Console.WriteLine("6- Submenú Reportes"  );
+                Console.WriteLine("7- Salir");
+
+                Console.WriteLine("Digite una opcion:" + (opcion = (int.Parse((Console.ReadLine())))));
+                switch (opcion)
+                {
+                    case 1:
+                        estudiante.InicializarVectores();
+                        break;
+                    case 2:
+                        estudiante.IncluirEstudiante();
+                        break;
+                    case 3: estudiante.ConsultarEstudiante(); ; break;
+                    case 4:
+                        estudiante.ModificarEstudiante();
+
+                        break;
+                    case 5: estudiante.BorrarEstudiante(); ;
+                        break;
+                    case 6:
+                        SubmenuReportes(); break;
+                    default:
+                        Console.WriteLine("Opcion Incorrecta");
+                        break;
+                }
+
+            } while (opcion !=7);
+
+        }
+
+
+
+        public static void SubmenuReportes()
+        {
+            int option;
+            do
+            {
+                Console.WriteLine("****** SUB MENU ******");
+                Console.WriteLine("1- Reporte Estudiantes por Condición");
+                Console.WriteLine("2- Reporte todos los datos");
+                Console.WriteLine("3- Regresar Menu Principal");
+
+                Console.WriteLine("Digite una opcion:" + (option = (int.Parse((Console.ReadLine())))));
+                switch (option)
+                {
+                    case 1:
+
+
+                        estudiante.ReporteEstudianteCondicion();
+
+                        break;
+                    case 2:
+                        estudiante.ReporteEstudiante();
+                        break;
+                    case 3:
+                    
+                        principal();
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion Incorrecta");
+                        break;
+                }
+
+            } while (option !=4);
+
+        }
+    }
+
+}
+
